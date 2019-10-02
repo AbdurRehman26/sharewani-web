5464b3dad9704dff2ce04141b2423e6e4e7e90e2
Kazmi <abdur.rehman@daraz.pk>
2019-10-02 12:59:43 +0500

    removed extra moduels

--- resources/js/router/modules/admin.js
+++ resources/js/router/modules/admin.js
@@ -33,28 +33,7 @@ const adminRoutes = {
       component: () => import('@/views/role-permission/List'),
       name: 'RoleList',
       meta: { title: 'rolePermission', icon: 'role', permissions: ['manage permission'] },
-    },
-    {
-      path: 'articles/create',
-      component: () => import('@/views/articles/Create'),
-      name: 'CreateArticle',
-      meta: { title: 'createArticle', icon: 'edit', permissions: ['manage article'] },
-      hidden: true,
-    },
-    {
-      path: 'articles/edit/:id(\\d+)',
-      component: () => import('@/views/articles/Edit'),
-      name: 'EditArticle',
-      meta: { title: 'editArticle', noCache: true, permissions: ['manage article'] },
-      hidden: true,
-    },
-    {
-      path: 'articles',
-      component: () => import('@/views/articles/List'),
-      name: 'ArticleList',
-      meta: { title: 'articleList', icon: 'list', permissions: ['manage article'] },
-    },
-  ],
+    }],
 };
 
 export default adminRoutes;
