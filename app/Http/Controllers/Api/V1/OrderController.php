6676c29805135aa218cc8a6219c8a888a18a54af
Kazmi <abdur.rehman@daraz.pk>
2019-10-08 00:52:14 +0500

    done

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -8,19 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 
 
-class OrderController extends ApiResourceController{
+class OrderController extends ApiResourceController
+{
 
     public $_repository;
 
-    public function __construct(OrderRepository $repository){
+    public function __construct(OrderRepository $repository)
+    {
         $this->_repository = $repository;
     }
 
-    public function rules($value=''){
+    public function rules($value ='')
+    {
         $rules = [];
 
-        if($value == 'store'){
-
+        if ($value == 'store') {
             $rules['product_id'] =  'required';
             $rules['from_date'] =  'required';
             $rules['to_date'] =  'required';
@@ -28,42 +30,35 @@ class OrderController extends ApiResourceController{
 
         }
 
-        if ($value == 'update'){
+        if ($value == 'update') {
+            $rules['id'] =  'required';
+        }
 
+
+        if ($value == 'destroy') {
             $rules['id'] =  'required';
 
         }
 
-
-        if($value == 'destroy'){
-
+        if ($value == 'show') {
             $rules['id'] =  'required';
 
         }
 
-        if($value == 'show'){
-
-            $rules['id'] =  'required';
-
-        }
-
-        if($value == 'index'){
-
+        if ($value == 'index') {
             $rules['pagination'] =  'nullable|in:true,false';
 
         }
 
         return $rules;
-
     }
 
-    public function input($value=''){
-
+    public function input ($value='') {
+        \Log::info('here');
         $input = request()->only('id', 'product_id', 'from_date', 'to_date', 'number_of_items');
         $input['user_id'] = request()->user() ?? null;
 
         return $input;
-
     }
 
     public function itemCount(Request $request)
@@ -77,7 +72,6 @@ class OrderController extends ApiResourceController{
         // HTTP_OK = 200;
 
         return response()->json($output, Response::HTTP_OK);
-
     }
 
 }
