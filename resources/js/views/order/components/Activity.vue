faa81703f271267ab83cea67f84fe6f19e9d9eb1
Kazmi <abdur.rehman@daraz.pk>
2019-10-30 23:33:01 +0500

    new build created

--- resources/js/views/order/components/Activity.vue
+++ resources/js/views/order/components/Activity.vue
@@ -1,6 +1,17 @@
 <template>
   <el-card v-if="item.product">
 
+    <h4>Order:</h4>
+
+    <list-inner-body class="order-container" :query="query" :items="[item]" />
+
+    <hr>
+    <hr>
+    <hr>
+    <hr>
+
+    <h4>Colliding Orders:</h4>
+
     <list-inner-body class="order-container" :query="query" :items="items" />
 
   </el-card>
@@ -34,7 +45,7 @@ export default {
   data() {
     return {
       query: {
-        total: 10,
+        total: 0,
         page: 1,
         limit: 15,
         keyword: '',
